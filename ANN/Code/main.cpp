ab1074ef473c7a73232f55108840cffb44b3f8b5
DeadFenris <theworkingfen@gmail.com>
2019-01-20 00:13:37 +0100

    Tests sketch

--- ANN/Code/main.cpp
+++ ANN/Code/main.cpp
@@ -4,21 +4,24 @@
 #include <random>
 #include "mlp.hpp"
 #include "load.hpp"
+#include "test.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     Load load;
-    std::vector<Data*> input;
+	Test test;
 
     if(argc > 1){
 	    std::string file(argv[1]);
 	    input = load.read(file);
     }
     else input = load.read("../Data/wdbc.data");
-
-
+	
+	test.makeTest(30, {5, 1}, 285, 2000);
+	
+	/*std::vector<Data*> input;
     mlp first(30, {5, 1});
 
     default_random_engine rand_num{static_cast<long unsigned int>(chrono::high_resolution_clock::now().time_since_epoch().count())};
@@ -37,62 +40,9 @@ int main(int argc, char* argv[])
         }
         */
 
-        //////////// Skalowanie danych wejściowych ////////////
-        
-        /*for(int j = 0; j < 2; j++)                                                          
-        {
-            first.setInput(j, (input[chosenExample]->getFeature(j))/10);
-        }*/
-        first.setInput(0, (input[chosenExample]->getFeature(0))/100);
-	first.setInput(1, (input[chosenExample]->getFeature(1))/100);
-        first.setInput(2, (input[chosenExample]->getFeature(2))/100);
-        first.setInput(3, (input[chosenExample]->getFeature(3))/100);
-        /*for(int j = 4; j < 9; j++)                                                          
-        {
-            first.setInput(j, (input[chosenExample]->getFeature(j))*10);
-        }*/
-	first.setInput(4, (input[chosenExample]->getFeature(4))*10);
-	first.setInput(5, (input[chosenExample]->getFeature(5))*10);
-	first.setInput(6, (input[chosenExample]->getFeature(6))*100);
-	first.setInput(7, (input[chosenExample]->getFeature(7))*100);
-        first.setInput(8, (input[chosenExample]->getFeature(8))*10);
-        first.setInput(9, (input[chosenExample]->getFeature(9))*100);
-        /*for(int j = 10; j < 13; j++)                                                          
-        {
-            first.setInput(j, input[chosenExample]->getFeature(j));
-        }*/
-	first.setInput(10, (input[chosenExample]->getFeature(10))*10);
-	first.setInput(11, (input[chosenExample]->getFeature(11)));
-	first.setInput(12, (input[chosenExample]->getFeature(12))/10);
-        first.setInput(13, (input[chosenExample]->getFeature(13))/100);
-        first.setInput(14, (input[chosenExample]->getFeature(14))*100);
-        /*for(int j = 15; j < 19; j++)                                                          
-        {
-            first.setInput(j, (input[chosenExample]->getFeature(j))*100);
-        }*/
-	first.setInput(15, (input[chosenExample]->getFeature(15))*100);
-	first.setInput(16, (input[chosenExample]->getFeature(16))*100);
-	first.setInput(17, (input[chosenExample]->getFeature(17))*100);
-	first.setInput(18, (input[chosenExample]->getFeature(18))*100);
-        first.setInput(19, (input[chosenExample]->getFeature(19))*1000);
-        first.setInput(20, (input[chosenExample]->getFeature(20))/100);
-        first.setInput(21, (input[chosenExample]->getFeature(21))/100);
-        first.setInput(22, (input[chosenExample]->getFeature(22))/1000);
-        first.setInput(23, (input[chosenExample]->getFeature(23))/1000);
-        /*for(int j = 24; j < 30; j++)                                                          
-        {
-            first.setInput(j, (input[chosenExample]->getFeature(j))*10);
-        }*/
-	first.setInput(24, (input[chosenExample]->getFeature(24))*10);
-	first.setInput(25, (input[chosenExample]->getFeature(25))*10);
-	first.setInput(26, (input[chosenExample]->getFeature(26))*10);
-	first.setInput(27, (input[chosenExample]->getFeature(27))*100);
-	first.setInput(28, (input[chosenExample]->getFeature(28))*10);
-	first.setInput(29, (input[chosenExample]->getFeature(29))*10);
-        
-        ////////////////////////////////////////////////////////////////////////////////////
+        //test.scale(first, input, chosenExample);
 
-        if(input[chosenExample]->getDiagnosis()=='M')
+        /*if(input[chosenExample]->getDiagnosis()=='M')
             first.setExpectedOutput(0, 1);
         else
             first.setExpectedOutput(0, 0);
@@ -111,7 +61,7 @@ int main(int argc, char* argv[])
         {
             first.setInput(j, (input[i]->getFeature(j))/10);
         }*/
-	first.setInput(0, (input[i]->getFeature(0))/100);
+	/*first.setInput(0, (input[i]->getFeature(0))/100);
 	first.setInput(1, (input[i]->getFeature(1))/100);
         first.setInput(2, (input[i]->getFeature(2))/100);
         first.setInput(3, (input[i]->getFeature(3))/100);
@@ -119,7 +69,7 @@ int main(int argc, char* argv[])
         {
             first.setInput(j, (input[i]->getFeature(j))*10);
         }*/
-	first.setInput(4, (input[i]->getFeature(4))*10);
+	/*first.setInput(4, (input[i]->getFeature(4))*10);
 	first.setInput(5, (input[i]->getFeature(5))*10);
 	first.setInput(6, (input[i]->getFeature(6))*100);
 	first.setInput(7, (input[i]->getFeature(7))*100);
@@ -129,7 +79,7 @@ int main(int argc, char* argv[])
         {
             first.setInput(j, input[i]->getFeature(j));
         }*/
-	first.setInput(10, (input[i]->getFeature(10))*10);
+	/*first.setInput(10, (input[i]->getFeature(10))*10);
 	first.setInput(11, (input[i]->getFeature(11)));
 	first.setInput(12, (input[i]->getFeature(12))/10);
         first.setInput(13, (input[i]->getFeature(13))/100);
@@ -138,7 +88,7 @@ int main(int argc, char* argv[])
         {
             first.setInput(j, (input[i]->getFeature(j))*100);
         }*/
-	first.setInput(15, (input[i]->getFeature(15))*100);
+	/*first.setInput(15, (input[i]->getFeature(15))*100);
 	first.setInput(16, (input[i]->getFeature(16))*100);
 	first.setInput(17, (input[i]->getFeature(17))*100);
 	first.setInput(18, (input[i]->getFeature(18))*100);
@@ -151,7 +101,7 @@ int main(int argc, char* argv[])
         {
             first.setInput(j, (input[i]->getFeature(j))*10);
         }*/
-	first.setInput(24, (input[i]->getFeature(24))*10);
+	/*first.setInput(24, (input[i]->getFeature(24))*10);
 	first.setInput(25, (input[i]->getFeature(25))*10);
 	first.setInput(26, (input[i]->getFeature(26))*10);
 	first.setInput(27, (input[i]->getFeature(27))*100);
---
+++ ANN/Code/test.hpp
@@ -0,0 +1,87 @@
+#include <string>
+#include <cstdlib>
+
+class Test{
+private:
+	
+	void scale(mlp network, std::vector<Data*> input, unsigned index){
+		/////////////////////////// Skalowanie danych wejściowych //////////////////////////
+        
+        network.setInput(0, (input[index]->getFeature(0))/100);
+		network.setInput(1, (input[index]->getFeature(1))/100);
+        network.setInput(2, (input[index]->getFeature(2))/100);
+        network.setInput(3, (input[index]->getFeature(3))/100);
+		network.setInput(4, (input[index]->getFeature(4))*10);
+		network.setInput(5, (input[index]->getFeature(5))*10);
+		network.setInput(6, (input[index]->getFeature(6))*100);
+		network.setInput(7, (input[index]->getFeature(7))*100);
+        network.setInput(8, (input[index]->getFeature(8))*10);
+        network.setInput(9, (input[index]->getFeature(9))*100);
+		network.setInput(10, (input[index]->getFeature(10))*10);
+		network.setInput(11, (input[index]->getFeature(11)));
+		network.setInput(12, (input[index]->getFeature(12))/10);
+        network.setInput(13, (input[index]->getFeature(13))/100);
+        network.setInput(14, (input[index]->getFeature(14))*100);
+		network.setInput(15, (input[index]->getFeature(15))*100);
+		network.setInput(16, (input[index]->getFeature(16))*100);
+		network.setInput(17, (input[index]->getFeature(17))*100);
+		network.setInput(18, (input[index]->getFeature(18))*100);
+        network.setInput(19, (input[index]->getFeature(19))*1000);
+        network.setInput(20, (input[index]->getFeature(20))/100);
+        network.setInput(21, (input[index]->getFeature(21))/100);
+        network.setInput(22, (input[index]->getFeature(22))/1000);
+        network.setInput(23, (input[index]->getFeature(23))/1000);
+		network.setInput(24, (input[index]->getFeature(24))*10);
+		network.setInput(25, (input[index]->getFeature(25))*10);
+		network.setInput(26, (input[index]->getFeature(26))*10);
+		network.setInput(27, (input[index]->getFeature(27))*100);
+		network.setInput(28, (input[index]->getFeature(28))*10);
+		network.setInput(29, (input[index]->getFeature(29))*10);
+        
+        ////////////////////////////////////////////////////////////////////////////////////
+	}
+	
+public:
+
+	void makeTest(unsigned inputAmount, vector<int> layers, unsigned noInstances, unsigned challangesNo){
+		std::vector<Data*> input;
+		mlp network(inputAmount, layers);
+
+		default_random_engine rand_num{static_cast<long unsigned int>(chrono::high_resolution_clock::now().time_since_epoch().count())};
+		uniform_real_distribution<> dis(0,noInstances-1);
+
+		int properlyClassified=0;
+
+		for(int i=0;i<challangesNo;i++)                           // uczenie sieci neuronowej na 2000 wybieranych losowo przykładach
+		{
+			int chosenExample=dis(rand_num);
+
+			scale(network, input, chosenExample);
+			
+			if(input[chosenExample]->getDiagnosis()=='M')
+				network.setExpectedOutput(0, 1);
+			else
+				network.setExpectedOutput(0, 0);
+
+			network.processDataAndLearn();  
+		}
+
+		for(int i=0;i<noInstances;i++)                                                                                                                              // sprawdzanie jakości nauki
+		{
+			scale(network, input, i);
+			
+			network.processData();
+			cout << input[i]->getDiagnosis() << " ";
+			cout << network.getOutputVector()[0];
+			cout << endl;
+
+			if((input[i]->getDiagnosis()=='M' && network.getOutputVector()[0]>=0.5) || (input[i]->getDiagnosis()=='B' && network.getOutputVector()[0]<0.5))
+				properlyClassified+=1;
+		}
+
+		cout << "Properly classified: " << properlyClassified << " in "<< noInstances <<" examples. That is " << ((double)properlyClassified/noInstances)*100 << " percent." << endl;
+	}
+	
+}
+	
+	
